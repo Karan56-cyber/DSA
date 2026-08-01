185311244159279cc5a6c8235ef113e8b78604d0
Karan Singh <karanjeet4560987@gmail.com>
2026-08-01 22:54:40 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (12.42%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,10 +1,40 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++){
-            nums[i]=nums[i]*nums[i];
+        int n = nums.size();
+        vector<int> result;
+
+        // Find the first non-negative number
+        int post = 0;
+        while (post < n && nums[post] < 0) {
+            post++;
         }
-        sort(nums.begin(),nums.end());
-        return nums;
+
+        int pre = post - 1;
+
+        // Merge the negative and non-negative parts
+        while (pre >= 0 && post < n) {
+            if (abs(nums[pre]) <= abs(nums[post])) {
+                result.push_back(nums[pre] * nums[pre]);
+                pre--;
+            } else {
+                result.push_back(nums[post] * nums[post]);
+                post++;
+            }
+        }
+
+        // Remaining negative numbers
+        while (pre >= 0) {
+            result.push_back(nums[pre] * nums[pre]);
+            pre--;
+        }
+
+        // Remaining non-negative numbers
+        while (post < n) {
+            result.push_back(nums[post] * nums[post]);
+            post++;
+        }
+
+        return result;
     }
 };
