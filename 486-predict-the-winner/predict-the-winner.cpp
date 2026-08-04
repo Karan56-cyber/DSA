111d933c5acdf7647f7fcdcdb5bf0f90f463ed8e
Karan Singh <karanjeet4560987@gmail.com>
2026-08-05 00:02:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (22.07%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     bool predictTheWinner(vector<int>& nums) {
+        vector<vector<int>>dp(nums.size(),vector<int>(nums.size(),-1));
         int total=0,player1=0;
         for(auto x:nums){
             total+=x;
         }
-        player1=solve(nums,0,nums.size()-1);
+        player1=solve(nums,0,nums.size()-1,dp);
         if(2*player1>=total){
             return true;
         }
         return false;
     }
-    int solve(vector<int>&nums,int i,int j){
+    int solve(vector<int>&nums,int i,int j,vector<vector<int>>&dp){
+        
         if(i>j){
             return 0;
         }
         if(i==j){
             return nums[i];
         }
-        int take_i=nums[i]+min(solve(nums,i+2,j),solve(nums,i+1,j-1));
-        int take_j=nums[j]+min(solve(nums,i+1,j-1),solve(nums,i,j-2));
-        return max(take_i,take_j);
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int take_i=nums[i]+min(solve(nums,i+2,j,dp),solve(nums,i+1,j-1,dp));
+        int take_j=nums[j]+min(solve(nums,i+1,j-1,dp),solve(nums,i,j-2,dp));
+        return dp[i][j]=max(take_i,take_j);
     }
 };
