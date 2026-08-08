941a310cdec7e1f80373630e30b2e0d567109e1c
Karan Singh <karanjeet4560987@gmail.com>
2026-08-09 00:24:45 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (66.76%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -15,7 +15,7 @@ public:
         // return ans;
         int ans=0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]>9 && nums[i]<99 || nums[i]>999 && nums[i]<=9999 || nums[i]>99999 && nums[i]<=999999){
+            if(nums[i]>9 && nums[i]<=99 || nums[i]>999 && nums[i]<=9999 || nums[i]>99999 && nums[i]<=999999){
                 ans+=1;
             }
         }
