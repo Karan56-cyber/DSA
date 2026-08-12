69bfa1c4fe1f080e04137d6d121214d9378f1d0d
Karan Singh <karanjeet4560987@gmail.com>
2026-08-12 11:07:49 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (42.18%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -11,19 +11,17 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode*temp=head;
-        int i=0;
-        unordered_map<int,int>mp;
-        while(temp!=nullptr){
-            mp[i]=temp->val;
-            temp=temp->next;
-            i++;
+        ListNode*curr=head;
+        ListNode*prev=nullptr;
+        ListNode*temp=nullptr;
+        while(curr!=nullptr){
+            temp=curr->next;
+          curr->next=prev;
+          
+          prev=curr;
+          curr=temp;
         }
-        temp=head;
-        for(int j=i-1;j>=0;j--){
-            temp->val=mp[j];
-            temp=temp->next;
-        }
-        return head;
+        
+        return prev;
     }
 };
