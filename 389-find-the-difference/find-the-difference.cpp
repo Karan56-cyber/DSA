483a2430d4cd05aa7431769214a0951cd11e2537
Karan Singh <karanjeet4560987@gmail.com>
2026-08-24 23:36:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (51.71%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+     int ans=0;
+     for(char a:s){
+        ans^=a;
+     }   
+     for(char a: t){
+        ans^=a;
+     }
+     return ans;
+    }
+};
